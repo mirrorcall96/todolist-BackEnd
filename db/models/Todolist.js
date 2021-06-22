41b6726f4b556599865c36e59fff0716da5fc8c8
mirrorcall96 <mirrorcall96@gmail.com>
2021-06-22 19:16:39 +0300

    meaningful message

---
+++ db/models/Todolist.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "ToDoList",
+    {
+      name: { type: DataTypes.STRING, allowNull: false },
+      status: { type: DataTypes.BOOLEAN, allowNull: false },
+      priority: { type: DataTypes.STRING, allowNull: false },
+      deadline: { type: DataTypes.DATEONLY, allowNull: false },
+    },
+    { createdAt: false, updatedAt: false }
+  );
+};
